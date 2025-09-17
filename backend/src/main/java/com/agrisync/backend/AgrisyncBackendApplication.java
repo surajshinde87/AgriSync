f42c5a3491489e63030618582c1903d0c76a03cd
Suraj Shinde <surajpshinde8767@gmail.com>
2025-09-17 22:34:17 +0530

    chore: initial Spring Boot project setup

---
+++ backend/src/main/java/com/agrisync/backend/AgrisyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agrisync.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrisyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrisyncBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/agrisync/backend/AgrisyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrisync.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrisyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
