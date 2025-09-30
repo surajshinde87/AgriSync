85c8518931deccbbf40f3d9dce41b609f8055fd9
Suraj Shinde <surajpshinde8767@gmail.com>
2025-09-30 22:27:26 +0530

    chore(frontend): setup React Vite + Tailwind v4 with custom theme and fonts

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+function App() {
+
+  return (
+   <>
+ <div className="flex items-center flex-col justify-center w-full h-screen bg-gradient-to-r from-primary via-secondary to-accent text-dark p-10">
+  <h1 className="text-8xl font-heading  m-auto my-10 drop-shadow-md">
+    Welcome to AgriSync
+  </h1>
+  <p className="text-3xl font-bold text-dark">
+     Connecting Farmers, Buyers, and Drivers with seamless logistics and marketplace features.
+  </p>
+  <button className="mt-6 bg-primary text-white px-6 py-3 rounded-lg hover:bg-green-700 transition">
+    Get Started
+  </button>
+</div>
+
+
+   </>
+  )
+}
+
+export default App
